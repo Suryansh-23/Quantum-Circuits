6ec862dd23fd646c3098df37c2f009a4c2212d28
Suryansh-23 <suryanshchauhan2005@gmail.com>
2021-08-25 03:32:33 +0530

    Fixing .gitignore issue

--- Exp#1/main.py
+++
@@ -1,9 +0,0 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-from numpy import pi
-
-qreg_q = QuantumRegister(1, "q")
-creg_c = ClassicalRegister(1, "c")
-circuit = QuantumCircuit(qreg_q, creg_c)
-
-circuit.z(qreg_q[0])
-circuit.measure(qreg_q[0], creg_c[0])
